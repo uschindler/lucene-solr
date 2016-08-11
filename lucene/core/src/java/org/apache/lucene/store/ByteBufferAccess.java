d4d48dff0007ce2ff7c77b53bb80bf98e8ff677c
Uwe Schindler <uschindler@apache.org>
2016-08-11 14:45:35 +0200

    Different variant using store barrier (like MutableCallSite)

--- lucene/core/src/java/org/apache/lucene/store/ByteBufferAccess.java
+++ lucene/core/src/java/org/apache/lucene/store/ByteBufferAccess.java
@@ -17,19 +17,17 @@
 package org.apache.lucene.store;
 
 import java.io.IOException;
-import java.lang.invoke.MethodHandle;
-import java.lang.invoke.MethodHandles;
-import java.lang.invoke.MethodType;
-import java.lang.invoke.SwitchPoint;
 import java.nio.ByteBuffer;
+import java.util.concurrent.atomic.AtomicInteger;
 
 final class ByteBufferAccess {
+  private static final AtomicInteger STORE_BARRIER = new AtomicInteger();
+  
   private final String resourceDescription;
   private final BufferCleaner cleaner;
-  private final SwitchPoint switchPoint;
-  private final MethodHandle mhGetBytesSafe, mhGetByteSafe, mhGetShortSafe, mhGetIntSafe, mhGetLongSafe,
-    mhGetPosByteSafe, mhGetPosShortSafe, mhGetPosIntSafe, mhGetPosLongSafe;
   
+  private boolean invalidated = false;
+
   /**
    * Pass in an implementation of this interface to cleanup ByteBuffers.
    * MMapDirectory implements this to allow unmapping of bytebuffers with private Java APIs.
@@ -42,167 +40,68 @@ final class ByteBufferAccess {
   ByteBufferAccess(String resourceDescription, BufferCleaner cleaner) {
     this.resourceDescription = resourceDescription;
     this.cleaner = cleaner;
-    if (cleaner != null) {
-      switchPoint = new SwitchPoint();
-      mhGetBytesSafe = switchPoint.guardWithTest(BYTEBUFFER_GET_BYTES_UNSAFE, BYTEBUFFER_GET_BYTES_FALLBACK);
-      mhGetByteSafe = switchPoint.guardWithTest(BYTEBUFFER_GET_BYTE_UNSAFE, BYTEBUFFER_GET_BYTE_FALLBACK);
-      mhGetShortSafe = switchPoint.guardWithTest(BYTEBUFFER_GET_SHORT_UNSAFE, BYTEBUFFER_GET_SHORT_FALLBACK);
-      mhGetIntSafe = switchPoint.guardWithTest(BYTEBUFFER_GET_INT_UNSAFE, BYTEBUFFER_GET_INT_FALLBACK);
-      mhGetLongSafe = switchPoint.guardWithTest(BYTEBUFFER_GET_LONG_UNSAFE, BYTEBUFFER_GET_LONG_FALLBACK);
-      mhGetPosByteSafe = switchPoint.guardWithTest(BYTEBUFFER_GET_POS_BYTE_UNSAFE, BYTEBUFFER_GET_POS_BYTE_FALLBACK);
-      mhGetPosShortSafe = switchPoint.guardWithTest(BYTEBUFFER_GET_POS_SHORT_UNSAFE, BYTEBUFFER_GET_POS_SHORT_FALLBACK);
-      mhGetPosIntSafe = switchPoint.guardWithTest(BYTEBUFFER_GET_POS_INT_UNSAFE, BYTEBUFFER_GET_POS_INT_FALLBACK);
-      mhGetPosLongSafe = switchPoint.guardWithTest(BYTEBUFFER_GET_POS_LONG_UNSAFE, BYTEBUFFER_GET_POS_LONG_FALLBACK);
-    } else {
-      switchPoint = null;
-      mhGetBytesSafe = BYTEBUFFER_GET_BYTES_UNSAFE;
-      mhGetByteSafe = BYTEBUFFER_GET_BYTE_UNSAFE;
-      mhGetShortSafe = BYTEBUFFER_GET_SHORT_UNSAFE;
-      mhGetIntSafe = BYTEBUFFER_GET_INT_UNSAFE;
-      mhGetLongSafe = BYTEBUFFER_GET_LONG_UNSAFE;
-      mhGetPosByteSafe = BYTEBUFFER_GET_POS_BYTE_UNSAFE;
-      mhGetPosShortSafe = BYTEBUFFER_GET_POS_SHORT_UNSAFE;
-      mhGetPosIntSafe = BYTEBUFFER_GET_POS_INT_UNSAFE;
-      mhGetPosLongSafe = BYTEBUFFER_GET_POS_LONG_UNSAFE;
-    }
   }
   
-  void invalidate(ByteBuffer bufs[]) throws IOException {
+  void invalidate(ByteBuffer... bufs) throws IOException {
     if (cleaner != null) {
-      // TODO: we should really batch this via deletePendingFiles() or similar, or perhaps
-      // queue up and take care asynchronously from another thread.
-      SwitchPoint.invalidateAll(new SwitchPoint[] { switchPoint });
+      invalidated = true;
+      // this should trigger a happens-before - so flushes all caches
+      STORE_BARRIER.lazySet(0);
       for (ByteBuffer b : bufs) {
         cleaner.freeBuffer(resourceDescription, b);
       }
     }
   }
   
-  ByteBuffer getBytes(ByteBuffer receiver, byte[] dst, int offset, int length) {
-    try {
-      return (ByteBuffer) mhGetBytesSafe.invokeExact(receiver, dst, offset, length);
-    } catch (Throwable e) {
-      rethrow(e);
-      throw new AssertionError();
+  private void ensureValid() {
+    if (invalidated) {
+      throw new NullPointerException();
     }
+  }
+  
+  void getBytes(ByteBuffer receiver, byte[] dst, int offset, int length) {
+    ensureValid();
+    receiver.get(dst, offset, length);
   }
   
   byte getByte(ByteBuffer receiver) {
-    try {
-      return (byte) mhGetByteSafe.invokeExact(receiver);
-    } catch (Throwable e) {
-      rethrow(e);
-      throw new AssertionError();
-    }
+    ensureValid();
+    return receiver.get();
   }
   
   short getShort(ByteBuffer receiver) {
-    try {
-      return (short) mhGetShortSafe.invokeExact(receiver);
-    } catch (Throwable e) {
-      rethrow(e);
-      throw new AssertionError();
-    }
+    ensureValid();
+    return receiver.getShort();
   }
   
   int getInt(ByteBuffer receiver) {
-    try {
-      return (int) mhGetIntSafe.invokeExact(receiver);
-    } catch (Throwable e) {
-      rethrow(e);
-      throw new AssertionError();
-    }
+    ensureValid();
+    return receiver.getInt();
   }
   
   long getLong(ByteBuffer receiver) {
-    try {
-      return (long) mhGetLongSafe.invokeExact(receiver);
-    } catch (Throwable e) {
-      rethrow(e);
-      throw new AssertionError();
-    }
+    ensureValid();
+    return receiver.getLong();
   }
   
   byte getByte(ByteBuffer receiver, int pos) {
-    try {
-      return (byte) mhGetPosByteSafe.invokeExact(receiver, pos);
-    } catch (Throwable e) {
-      rethrow(e);
-      throw new AssertionError();
-    }
+    ensureValid();
+    return receiver.get(pos);
   }
   
   short getShort(ByteBuffer receiver, int pos) {
-    try {
-      return (short) mhGetPosShortSafe.invokeExact(receiver, pos);
-    } catch (Throwable e) {
-      rethrow(e);
-      throw new AssertionError();
-    }
+    ensureValid();
+    return receiver.getShort(pos);
   }
   
   int getInt(ByteBuffer receiver, int pos) {
-    try {
-      return (int) mhGetPosIntSafe.invokeExact(receiver, pos);
-    } catch (Throwable e) {
-      rethrow(e);
-      throw new AssertionError();
-    }
+    ensureValid();
+    return receiver.getInt(pos);
   }
   
   long getLong(ByteBuffer receiver, int pos) {
-    try {
-      return (long) mhGetPosLongSafe.invokeExact(receiver, pos);
-    } catch (Throwable e) {
-      rethrow(e);
-      throw new AssertionError();
-    }
+    ensureValid();
+    return receiver.getLong(pos);
   }
-  
-  /** Hack to rethrow unknown Exceptions from {@link MethodHandle#invokeExact}: */
-  @SuppressWarnings("unchecked")
-  private static <T extends Throwable> void rethrow(Throwable t) throws T {
-      throw (T) t;
-  }
-
-  private static MethodHandle createFallback(MethodType type) {
-    MethodHandle fallback = MethodHandles.throwException(type.returnType(), NullPointerException.class).bindTo(new NullPointerException());
-    return MethodHandles.dropArguments(fallback, 0, type.parameterArray());
-  }
-  
-  private static final MethodHandle BYTEBUFFER_GET_BYTES_UNSAFE, BYTEBUFFER_GET_BYTES_FALLBACK,
-    BYTEBUFFER_GET_BYTE_UNSAFE, BYTEBUFFER_GET_BYTE_FALLBACK,
-    BYTEBUFFER_GET_SHORT_UNSAFE, BYTEBUFFER_GET_SHORT_FALLBACK,
-    BYTEBUFFER_GET_INT_UNSAFE, BYTEBUFFER_GET_INT_FALLBACK,
-    BYTEBUFFER_GET_LONG_UNSAFE, BYTEBUFFER_GET_LONG_FALLBACK,
-    BYTEBUFFER_GET_POS_BYTE_UNSAFE, BYTEBUFFER_GET_POS_BYTE_FALLBACK,
-    BYTEBUFFER_GET_POS_SHORT_UNSAFE, BYTEBUFFER_GET_POS_SHORT_FALLBACK,
-    BYTEBUFFER_GET_POS_INT_UNSAFE, BYTEBUFFER_GET_POS_INT_FALLBACK,
-    BYTEBUFFER_GET_POS_LONG_UNSAFE, BYTEBUFFER_GET_POS_LONG_FALLBACK;
-  static {
-    MethodHandles.Lookup lookup = MethodHandles.publicLookup();
-    try {
-      BYTEBUFFER_GET_BYTES_UNSAFE = lookup.findVirtual(ByteBuffer.class, "get", MethodType.methodType(ByteBuffer.class, byte[].class, int.class, int.class));
-      BYTEBUFFER_GET_BYTES_FALLBACK = createFallback(BYTEBUFFER_GET_BYTES_UNSAFE.type());
-      BYTEBUFFER_GET_BYTE_UNSAFE = lookup.findVirtual(ByteBuffer.class, "get", MethodType.methodType(byte.class));
-      BYTEBUFFER_GET_BYTE_FALLBACK = createFallback(BYTEBUFFER_GET_BYTE_UNSAFE.type());
-      BYTEBUFFER_GET_SHORT_UNSAFE = lookup.findVirtual(ByteBuffer.class, "getShort", MethodType.methodType(short.class));
-      BYTEBUFFER_GET_SHORT_FALLBACK = createFallback(BYTEBUFFER_GET_SHORT_UNSAFE.type());
-      BYTEBUFFER_GET_INT_UNSAFE = lookup.findVirtual(ByteBuffer.class, "getInt", MethodType.methodType(int.class));
-      BYTEBUFFER_GET_INT_FALLBACK = createFallback(BYTEBUFFER_GET_INT_UNSAFE.type());
-      BYTEBUFFER_GET_LONG_UNSAFE = lookup.findVirtual(ByteBuffer.class, "getLong", MethodType.methodType(long.class));
-      BYTEBUFFER_GET_LONG_FALLBACK = createFallback(BYTEBUFFER_GET_LONG_UNSAFE.type());
-      BYTEBUFFER_GET_POS_BYTE_UNSAFE = lookup.findVirtual(ByteBuffer.class, "get", MethodType.methodType(byte.class, int.class));
-      BYTEBUFFER_GET_POS_BYTE_FALLBACK = createFallback(BYTEBUFFER_GET_POS_BYTE_UNSAFE.type());
-      BYTEBUFFER_GET_POS_SHORT_UNSAFE = lookup.findVirtual(ByteBuffer.class, "getShort", MethodType.methodType(short.class, int.class));
-      BYTEBUFFER_GET_POS_SHORT_FALLBACK = createFallback(BYTEBUFFER_GET_POS_SHORT_UNSAFE.type());
-      BYTEBUFFER_GET_POS_INT_UNSAFE = lookup.findVirtual(ByteBuffer.class, "getInt", MethodType.methodType(int.class, int.class));
-      BYTEBUFFER_GET_POS_INT_FALLBACK = createFallback(BYTEBUFFER_GET_POS_INT_UNSAFE.type());
-      BYTEBUFFER_GET_POS_LONG_UNSAFE = lookup.findVirtual(ByteBuffer.class, "getLong", MethodType.methodType(long.class, int.class));
-      BYTEBUFFER_GET_POS_LONG_FALLBACK = createFallback(BYTEBUFFER_GET_POS_LONG_UNSAFE.type());
-    } catch (NoSuchMethodException | IllegalAccessException e) {
-      throw new Error(e);
-    }
-  }
-  
+    
 }
